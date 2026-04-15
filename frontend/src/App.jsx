d40fb81dfb4c6284a3cc79de6a1441d82957dc54
milankhanchi002 <milankhanchi@gmail.com>
2026-04-15 21:29:51 +0530

    deplyment config added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,28 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import {SignedIn ,SignIn, SignInButton, SignOutButton, SignedOut, UserButton} from "@clerk/clerk-react";
 
 function App() {
 
 
   return (
     <>
-      hello
+      <h1>Welcome to APP</h1>
+      <SignedOut>
+      <SignInButton mode="modal">
+        <button className="">Sign up please</button>
+      </SignInButton>
+      </SignedOut>
+
+      <SignedIn>
+        <SignOutButton/>
+      </SignedIn>
+
+      <UserButton/>
+      
+      
+      
     </>
   )
 }
