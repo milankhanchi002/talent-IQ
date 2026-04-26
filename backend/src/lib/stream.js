119f94d95a67f03bf665a00be153de45cfdf2cbf
milankhanchi002 <milankhanchi@gmail.com>
2026-04-27 02:51:35 +0530

    remove unneccesary import

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -1,29 +1,28 @@
-import {StreamChat} from "stream-chat";
+import { StreamChat } from "stream-chat";
 import { ENV } from "./env.js";
-import { use } from "react";
 
-const apiKey=ENV.STREAM_API_KEY;
-const apiSecret=ENV.STREAM_API_SECRET;
+const apiKey = ENV.STREAM_API_KEY;
+const apiSecret = ENV.STREAM_API_SECRET;
 
-if(!apiKey || !apiSecret){
+if (!apiKey || !apiSecret) {
     throw new Error("STREAM_API_KEY and STREAM_API_SECRET must be defined in .env file");
-    // console.log("STREAM_API_KEY and STREAM_API_SECRET must be defined in .env file");
-    
 }
 
 export const chatClient = StreamChat.getInstance(apiKey, apiSecret);
-export const upsertStreamUser=async(userData)=>{
-    try{
-        await chatClient.upsertUser(userData)
-        return userData
+
+export const upsertStreamUser = async (userData) => {
+    try {
+        await chatClient.upsertUser(userData);
+        return userData;
     } catch (error) {
-        console.error("Error upserting stream user:", error)
+        console.error("Error upserting stream user:", error);
     }
-}
-export const deleteStreamUser=async(userId)=>{
-    try{
-        await chatClient.deleteUser(userId)
+};
+
+export const deleteStreamUser = async (userId) => {
+    try {
+        await chatClient.deleteUser(userId);
     } catch (error) {
-        console.error("Error deleting stream user:", error)
+        console.error("Error deleting stream user:", error);
     }
-}
+};
